6b3b3616dbbdf126496800e6e70c54f57f0fc9ff
zedyang <zedyang@tencent.com>
2021-02-04 11:12:16 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # css-compatible-loader
+
+webpack loader，用于css适配。
---
+++ index.js
@@ -0,0 +1,16 @@
+import loaderUtils from 'loader-utils'
+
+module.exports = function(source) {
+  const options = loaderUtils.getOptions(this)
+  const scaleUnit = options.scale / 10
+  const regpx = /(\d+)(px)/g
+  const regPX = /(\d+)(PX)/g
+  let nextSource = source.replace(regpx, (match, p1) => {
+    return (p1 / scaleUnit ) + 'rem'
+  })
+
+  nextSource = nextSource.replace(regPX, (match, p1) => {
+    return p1 + 'px'
+  })
+  return nextSource
+}
